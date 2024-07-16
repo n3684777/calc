05a5174cf2b57535f54d96833ae73700d599e7bc
n3684777 <n3684777@gmail.com>
2024-07-16 16:07:12 +0800

    Update index.js

--- jstest/index.js
+++ jstest/index.js
@@ -18,7 +18,7 @@ Object.defineProperties(obj, {
   setStart: {
     value: function () {
       console.log(
-        "monkey A / monkey B / water / box tissue / bag tissue  (1,2,3) start"
+        "monkey A / monkey B / water / box tissue / bag tissue  (1,2,3,storage) start"
       );
       this.moka_start1 = scanf("%d");
       this.mokb_start1 = scanf("%d");
@@ -35,12 +35,18 @@ Object.defineProperties(obj, {
       this.water_start3 = scanf("%d");
       this.boxtis_start3 = scanf("%d");
       this.bagtis_start3 = scanf("%d");
+
+      this.moka_storage_start = scanf("%d");
+      this.mokb_storage_start = scanf("%d");
+      this.water_storage_start = scanf("%d");
+      this.boxtis_storage_start = scanf("%d");
+      this.bagtis_storage_start = scanf("%d");
     },
   },
   setEnd: {
     value: function () {
       console.log(
-        "monkey A / monkey B / water / box tissue / bag tissue  (1,2,3) end"
+        "monkey A / monkey B / water / box tissue / bag tissue  (1,2,3,storage) end"
       );
       this.moka_end1 = scanf("%d");
       this.mokb_end1 = scanf("%d");
@@ -57,6 +63,12 @@ Object.defineProperties(obj, {
       this.water_end3 = scanf("%d");
       this.boxtis_end3 = scanf("%d");
       this.bagtis_end3 = scanf("%d");
+
+      this.moka_storage_end = scanf("%d");
+      this.mokb_storage_end = scanf("%d");
+      this.water_storage_end = scanf("%d");
+      this.boxtis_storage_end = scanf("%d");
+      this.bagtis_storage_end = scanf("%d");
     },
   },
   calc: {
@@ -76,8 +88,12 @@ Object.defineProperties(obj, {
       console.log(
         `monkA endTotal = \x1b[31m${moka_end}\x1b[0m , monkA MorningTotal = \x1b[31m${this.moka_morningTotal}\x1b[0m`
       );
-      if (moka_end - this.moka_morningTotal == moka_start - moka_end) {
-        console.log(`\x1b[32m mok A amount ok!\x1b[0m`);
+      if (moka_end - this.moka_morningTotal == moka_end - moka_start) {
+        console.log(
+          `\x1b[32m mok A amount ok!\x1b[0m , amount is \x1b[32m${
+            moka_end - moka_start
+          }\x1b[0m`
+        );
       } else {
         console.log(
           `\x1b[31m mok A amount error\x1b[0m , in fact amount is ${
@@ -93,7 +109,11 @@ Object.defineProperties(obj, {
         `monkB endTotal = \x1b[31m${mokb_end}\x1b[0m , monkB MorningTotal = \x1b[31m${this.mokb_morningTotal}\x1b[0m`
       );
       if (mokb_end - this.mokb_morningTotal == mokb_end - mokb_start) {
-        console.log(`\x1b[32m mok B amount ok!\x1b[0m`);
+        console.log(
+          `\x1b[32m mok B amount ok!\x1b[0m , amount is \x1b[32m${
+            mokb_end - mokb_start
+          }\x1b[0m`
+        );
       } else {
         console.log(
           `\x1b[31m mok B amount error\x1b[0m , in fact amount is ${
@@ -105,10 +125,16 @@ Object.defineProperties(obj, {
           console.log(`mokb_start = ${mokb_start}`);
         }
       }
-      console.log(`water endTotal = \x1b[31m${water_end}\x1b[0m`);
+      console.log(
+        `water endTotal = \x1b[31m${water_end}\x1b[0m , water MorningTotal = \x1b[31m${this.water_morningTotal}\x1b[0m`
+      );
 
       if (water_end - this.water_morningTotal == water_end - water_start) {
-        console.log(`\x1b[32m water amount ok!\x1b[0m`);
+        console.log(
+          `\x1b[32m water amount ok!\x1b[0m , amount is \x1b[32m${
+            water_end - water_start
+          }\x1b[0m]`
+        );
       } else {
         console.log(
           `\x1b[31m water amount error\x1b[0m , in fact amount is ${
@@ -121,10 +147,16 @@ Object.defineProperties(obj, {
         }
       }
 
-      console.log(`box tissue endTotal = \x1b[31m${boxtis_end}\x1b[0m`);
+      console.log(
+        `box tissue endTotal = \x1b[31m${boxtis_end}\x1b[0m , box tissue MorningTotal = \x1b[31m${this.boxtis_morningTotal}\x1b[0m`
+      );
 
       if (boxtis_end - this.boxtis_morningTotal == boxtis_end - boxtis_start) {
-        console.log(`\x1b[32m box tissue amount ok!\x1b[0m`);
+        console.log(
+          `\x1b[32m box tissue amount ok!\x1b[0m , amount is \x1b[32m${
+            boxtis_end - boxtis_start
+          }\x1b[0m]`
+        );
       } else {
         console.log(
           `\x1b[31m boxtis amount error\x1b[0m , in fact amount is ${
@@ -137,10 +169,16 @@ Object.defineProperties(obj, {
         }
       }
 
-      console.log(`bag tissue endTotal = \x1b[31m${bagtis_end}\x1b[0m`);
+      console.log(
+        `bag tissue endTotal = \x1b[31m${bagtis_end}\x1b[0m , bag tissue MorningTotal = \x1b[31m${this.bagtis_morningTotal}\x1b[0m`
+      );
 
       if (bagtis_end - this.bagtis_morningTotal == bagtis_end - bagtis_start) {
-        console.log(`\x1b[32m bag tissue amount ok!\x1b[0m`);
+        console.log(
+          `\x1b[32m bag tissue amount ok!\x1b[0m , amount is \x1b[32m${
+            bagtis_end - bagtis_start
+          }\x1b[0m`
+        );
       } else {
         console.log(
           `\x1b[31m bag tissue amount error\x1b[0m , in fact amount is ${
@@ -157,23 +195,56 @@ Object.defineProperties(obj, {
   Total: {
     value: function () {
       let mokA_startTotal =
-        this.moka_start1 + this.moka_start2 + this.moka_start3;
+        this.moka_start1 +
+        this.moka_start2 +
+        this.moka_start3 +
+        this.moka_storage_start;
       let mokB_startTotal =
-        this.mokb_start1 + this.mokb_start2 + this.mokb_start3;
+        this.mokb_start1 +
+        this.mokb_start2 +
+        this.mokb_start3 +
+        this.mokb_storage_start;
       let water_startTotal =
-        this.water_end1 + this.water_start2 + this.water_start3;
+        this.water_start1 +
+        this.water_start2 +
+        this.water_start3 +
+        this.water_storage_start;
       let boxtis_startTotal =
-        this.boxtis_start1 + this.boxtis_start2 + this.boxtis_start3;
+        this.boxtis_start1 +
+        this.boxtis_start2 +
+        this.boxtis_start3 +
+        this.boxtis_storage_start;
       let bagtis_startTotal =
-        this.bagtis_start1 + this.bagtis_start2 + this.bagtis_start3;
+        this.bagtis_start1 +
+        this.bagtis_start2 +
+        this.bagtis_start3 +
+        this.bagtis_storage_start;
 
-      let mokA_endTotal = this.moka_end1 + this.moka_end2 + this.moka_end3;
-      let mokB_endTotal = this.mokb_end1 + this.mokb_end2 + this.mokb_end3;
-      let water_endTotal = this.water_end1 + this.water_end2 + this.water_end3;
+      let mokA_endTotal =
+        this.moka_end1 +
+        this.moka_end2 +
+        this.moka_end3 +
+        this.moka_storage_end;
+      let mokB_endTotal =
+        this.mokb_end1 +
+        this.mokb_end2 +
+        this.mokb_end3 +
+        this.mokb_storage_end;
+      let water_endTotal =
+        this.water_end1 +
+        this.water_end2 +
+        this.water_end3 +
+        this.water_storage_end;
       let boxtis_endTotal =
-        this.boxtis_end1 + this.boxtis_end2 + this.boxtis_end3;
+        this.boxtis_end1 +
+        this.boxtis_end2 +
+        this.boxtis_end3 +
+        this.boxtis_storage_end;
       let bagtis_endTotal =
-        this.bagtis_end1 + this.bagtis_end2 + this.bagtis_end3;
+        this.bagtis_end1 +
+        this.bagtis_end2 +
+        this.bagtis_end3 +
+        this.bagtis_storage_end;
       return [
         mokA_endTotal,
         mokB_endTotal,
